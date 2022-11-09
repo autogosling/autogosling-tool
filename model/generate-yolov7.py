1beae4ea5a35c6f11b9b8649daa3c1d2e591f7eb
mnqng <54530709+mnqng@users.noreply.github.com>
2022-11-09 10:52:23 -0500

    Format dataset into yolov7 dataset format

--- model/generate-yolov7.py
+++ model/generate-yolov7.py
@@ -58,6 +58,12 @@ yolov7_configuration = {
 IMAGES = "images"
 BBOX = "bbox"
 LABELS = "labels"
+
+def load_mapping(mapping_fn="data/class_mapping.json"):
+    with open(mapping_fn,"r") as f:
+        return json.load(f)
+
+CLASS_MAPPING = load_mapping()
 def make_folders_if_not_exist(folder_path):
     if not os.path.isdir(folder_path):
         os.makedirs(folder_path)
@@ -77,37 +83,53 @@ def copy_images(split_folder,output_folder,mode):
         size_dict[stem] = (full_width, full_height)
     return size_dict
 
-def convert_txt(json_path,full_w,full_h):
+def get_class_ind(string):
+    return CLASS_MAPPING.get(string,-1)
+
+def convert_txt(json_path,marks_content, full_w,full_h):
     data = []
     with open(json_path,"r") as f:
         data = json.load(f)
-    def parse_item(item):
+    def parse_item(item,mark):
         cx,cy,w,h = None, None, None, None
         category = 0
         if "width" in item: # then this is rectangular
             x,y,w,h = item["x"], item["y"], item['width'], item['height']
             cx, cy = x + w/2, y + h/2
-            category = 0
+            category = "linear"
         elif "outerRadius" in item: # then it is circular
             cx, cy = item['cx'], item['cy']
             w = h = item['outerRadius']
-            category = 1
+            category = "circular"
         else:
             return None
         cx /= full_w
         cy /= full_h
         w /= full_w
         h /= full_h
-        all_strs = [str(el) for el in [category,cx,cy,w,h]]
-        return ' '.join(all_strs)
-        
-    content = '\n'.join(set(filter(lambda el: el is not None,[parse_item(item) for item in data])))
+
+        category_ind = get_class_ind(category)
+        mark_ind = get_class_ind(mark)
+        rest_data = ' '.join([str(el) for el in [cx,cy,w,h]])
+        inds = [category_ind, mark_ind]
+        return [f"{ind} {rest_data}" for ind in inds if ind != -1]
+
+    nested_lists = filter(lambda el: el is not None,[parse_item(item,mark) for item,mark in zip(data,marks_content)])
+    flattened_set = set([el for sublist in nested_lists for el in sublist])
+    content = '\n'.join(flattened_set)
     return content
     
 
 def write_txt(fn,content):
     with open(fn,"w") as f:
         f.write(content)
+
+def read_marks(stem_path,split_folder,mode):
+    complete_path = pjoin(split_folder, mode,"marks", stem_path + ".json")
+    marks = []
+    with open(complete_path,"r") as f:
+        marks = json.load(f)
+    return marks
 
 def copy_and_convert_labels(split_folder,output_folder,mode,img_sizes):
     src_labels = pjoin(split_folder,mode,BBOX)
@@ -120,7 +142,8 @@ def copy_and_convert_labels(split_folder,output_folder,mode,img_sizes):
         dest_end = stem_path + ".txt"
         full_w, full_h = img_sizes[stem_path]
         complete_dest_label_path = pjoin(dest_labels,dest_end)
-        txt_content = convert_txt(complete_src_label_path, full_w, full_h)
+        marks_content = read_marks(stem_path,split_folder,mode)
+        txt_content = convert_txt(complete_src_label_path,marks_content, full_w, full_h)
         write_txt(complete_dest_label_path,txt_content)
 
 
