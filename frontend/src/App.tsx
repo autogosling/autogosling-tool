e6d77a9183df5d70e73e0c1e5d5651d125b1ffad
mq-liang <mnqngliang@gmail.com>
2023-01-16 23:40:14 +0000

    add stepper to UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,17 +6,29 @@ import {GoslingEditorPre, DEFAULT_SPEC} from './GoslingEditorPre';
 // import {GoslingComponent} from "gosling.js";
 import GoslingSketch from "./GoslingSketch"
 import {EX_SPEC_BASIC_SEMANTIC_ZOOM} from "./default_specs";
+import Box from '@mui/material/Box';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
-
-
-function DataResult({ data } : {data : any}) {
+function DataResult({ data, step } : {data : any, step : Number}) {
   const {tracks_info : tracksInfo, image, spec, width, height} = data
+  if (step === 0) {
+    return       <GoslingSketch image={image} tracksInfo={tracksInfo} width={width} height={height}/>
+  }
+  if (step === 1) {
+    return <GoslingEditorPre spec={JSON.stringify(spec)}/>;
+  }
   return (
     <div>
-      <h2>YoloV7's predictions of the charts' shape</h2>
+      {/*<h2>YoloV7's predictions of the charts' shape</h2>*/}
       {/* <img src={image} /> */}
-      <GoslingSketch image={image} tracksInfo={tracksInfo} width={width} height={height}/>
-      <GoslingEditorPre spec={JSON.stringify(spec)}/>
       <pre>{JSON.stringify(tracksInfo, null, 2)}</pre>
     </div>)
 }
@@ -24,6 +36,9 @@ function App() {
   const [hasData, setHasData] = useState(false)
   const [data, setData] = useState()
   const [error, setError] = useState(false)
+  const [activeStep, setActiveStep] = useState(0)
+  
+  
   // const gosRef = React.useRef(null)
   const handleSubmit = async (e: any) => {
     e.preventDefault()
@@ -39,6 +54,9 @@ function App() {
     setHasData(true)
   }
   
+  const handleNavigation = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep === 0 ? 1 : 0);
+  }
 
   return (
     <div className="App">
@@ -49,8 +67,27 @@ function App() {
         {/* <input type="file" name="json" id="image" /> */}
         <button type="submit">Submit image</button>
       </form>
+      <Box sx={{ width: '100%' }}>
+        <Stepper activeStep={activeStep}>
+
+          <Step key={0}>
+            <Typography>
+              <StepLabel>Detection</StepLabel>
+            </Typography>
+          </Step>
+          <Step key={1}>
+          <Typography>
+              <StepLabel>Reconstruction</StepLabel>
+            </Typography>
+          </Step>
+        </Stepper>
+        
+        <Button onClick={handleNavigation}>
+          {activeStep === 1 ? 'Back' : 'Next'}
+        </Button>
+      </Box>
       <br />
-      {(hasData && !error) && <DataResult data={data} />}
+      {(hasData && !error) && <DataResult data={data} step={activeStep} />}
     </div>
   );
 }
