1b9e6e06ce3fca9dcffa407682248669592188ad
mq-liang <mnqngliang@gmail.com>
2023-02-21 16:47:07 +0000

    feat: added multiple mark selection

--- frontend/src/GoslingTable.js
+++ frontend/src/GoslingTable.js
@@ -11,9 +11,12 @@ import TextField from "@mui/material/TextField";
 import InputLabel from '@mui/material/InputLabel';
 import FormControl from '@mui/material/FormControl';
 import NativeSelect from '@mui/material/NativeSelect';
+import MenuItem from '@mui/material/MenuItem';
+import OutlinedInput from '@mui/material/OutlinedInput';
 
 import { GROUND_VIZ_BACKEND_URL, VIZ_BACKEND_URL } from './Config';
-import { layoutOptions,orientOptions } from "./utils";
+import { layoutOptions,orientOptions, chartOptions } from "./utils";
+import { Select } from "@mui/material";
 
 
 const properties = ["x", "y", "width", "height", "layout", "mark", "orientation"]
@@ -21,13 +24,20 @@ const singleSelectProperties = ["layout", "orientation"]
 const multiSelectProperties = ["mark"]
 const options = {
     "layout": layoutOptions,
-    "orientation":orientOptions
+    "orientation":orientOptions,
+    "mark":chartOptions
 }
 
 const EditableTableCell = ({value,handler,editMode,property}) => {
     const handleChange = (e) => {
         const newValue = e.target.value
         handler(newValue)
+    }
+    const handleMultipleChange = (e)=>{
+        const {
+            target:{value},
+        } = e;
+        handler(typeof value === 'string' ? value.split(','):value,);
     }
     if (!editMode){
         return <TableCell>
@@ -41,6 +51,7 @@ const EditableTableCell = ({value,handler,editMode,property}) => {
                 </InputLabel>
                 <NativeSelect
                 defaultValue={value}
+                onChange={handleChange}
                 inputProps={{
                     name: 'value',
                     id: 'uncontrolled-native',
@@ -48,6 +59,26 @@ const EditableTableCell = ({value,handler,editMode,property}) => {
                 >
                 {options[property].map(x=><option value={x}>{x}</option>)}
                 </NativeSelect>
+            </FormControl>
+        </TableCell>
+    } else if (multiSelectProperties.includes(property)){
+        return <TableCell>
+            <FormControl fullWidth>
+                <InputLabel variant="standard" htmlFor="uncontrolled-native">
+                {property}
+                </InputLabel>
+                <Select 
+                multiple
+                value={value}
+                name="multiple-select"
+                onChange = {handleMultipleChange}
+                input={<OutlinedInput label={property} />}
+                >
+                    {options[property].map(x=>
+                    <MenuItem key={x} value={x}>
+                        {x}
+                    </MenuItem>)}
+                </Select>
             </FormControl>
         </TableCell>
     } else {
--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -23,4 +23,4 @@ const layoutOptions = [
     "circular"
 ]
 
-export {orientOptions, layoutOptions};
+export {orientOptions, layoutOptions, chartOptions};
